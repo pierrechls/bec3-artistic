f50424ad09667ffedb1d70000cb0ee58cc14cb53
climoge <corentin.limoge@gmail.com>
2016-01-15 14:58:37 +0100

    Dev du client REST

--- project/template/main.cpp
+++ project/template/main.cpp
@@ -1,14 +1,60 @@
 #include <glimac/SDLWindowManager.hpp>
 #include <GL/glew.h>
 #include <iostream>
+#include <curl/curl.h>
 
 #include "Engine.hpp"
 
 using namespace std;
 using namespace glimac;
 
+#include <string>
 
-int main(int argc, char *argv[])
+#include "curl_easy.h"
+#include "curl_pair.h"
+#include "curl_form.h"
+#include "curl_exception.h"
+
+#include "restclient-cpp/restclient.h"
+
+using std::string;
+
+using curl::curl_form;
+using curl::curl_easy;
+using curl::curl_pair;
+using curl::curl_header;
+using curl::curl_easy_exception;
+using curl::curlcpp_traceback;
+
+int main(int argc, const char **argv) {
+	RestClient::headermap headers;
+	headers["Content-Type"] = "application/json";
+	RestClient::response connect = RestClient::post("http://localhost:9000/login", "text/json", "{\"username\":\"corentin.limoge\",\"password\":\"coucou\",\"service\":\"im.bec3.com\",\"resource\":\"REST\"}", headers);
+	cout << connect.code << endl;
+	RestClient::response r = RestClient::get("http://localhost:9000/feature/LIGHT", connect.headers);
+	cout << r.code << endl;
+	cout << r.body << endl;
+	/*RestClient::response r = RestClient::post("http://url.com/post", "text/json", "{\"foo\": \"bla\"}");
+	RestClient::response r = RestClient::put("http://url.com/put", "text/json", "{\"foo\": \"bla\"}");
+	RestClient::response r = RestClient::del("http://url.com/delete");
+
+	// add some headers
+
+	RestClient::headermap headers;
+	headers["Accept"] = "application/json";
+
+	RestClient::response r = RestClient::get("http://url.com", headers);
+	RestClient::response r = RestClient::post("http://url.com/post", "text/json", "{\"foo\": \"bla\"}", headers);
+	RestClient::response r = RestClient::put("http://url.com/put", "text/json", "{\"foo\": \"bla\"}", headers);
+	RestClient::response r = RestClient::del("http://url.com/delete", headers);
+
+	RestClient::setAuth("WarMachine68", "WARMACHINEROX");
+	curl_pair<CURLformoption,string> name_cont(CURLFORM_COPYCONTENTS,"{\"username\":\"corentin.limoge\",\"password\":\"coucou\",\"service\":\"im.bec3.com\",\"resource\":\"REST\"}");*/
+    return 0;
+}
+
+
+/*int main(int argc, char *argv[])
 {
     
     // Initialize SDL and open a window
@@ -46,6 +92,6 @@ int main(int argc, char *argv[])
     //Artistic.stop();
 
     return 0;
-}
+}*/
 
 
